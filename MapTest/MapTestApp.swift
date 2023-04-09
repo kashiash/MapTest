3e61dc9ace7cfa56d8319d010cc69b5fbcb25b39
Jacek Kosinski <kashiash@gmail.com>
2023-04-09 08:22:25 +0200

    Initial Commit

---
+++ MapTest/MapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestApp.swift
+//  MapTest
+//
+//  Created by Jacek Kosiński G on 09/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
